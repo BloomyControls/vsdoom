39382bd1f0ab28f077e1fd7c42002f1668ef1bf8
Will Eccles <william.r.eccles@bloomy.com>
2023-06-16 14:28:59 -0400

    i_fb_video.c: fix bugs and whitespace

--- src/device/i_fb_video.c
+++ src/device/i_fb_video.c
@@ -9,7 +9,7 @@
 #include <stdint.h>
 #include "v_video.h"
 
-static FILE* fbfd = 0;
+static int fbfd = 0;
 static struct fb_var_screeninfo vinfo;
 static struct fb_fix_screeninfo finfo;
 static long int screensize = 0;
@@ -21,7 +21,7 @@ void I_InitGraphics (void)
 {
     /* Open the file for reading and writing */
     fbfd = open("/dev/fb0", O_RDWR);
-    if (!fbfd) {
+    if (fbfd == -1) {
             printf("Error: cannot open framebuffer device.\n");
             exit(1);
     }
@@ -30,28 +30,28 @@ void I_InitGraphics (void)
     /* Get fixed screen information */
     if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo)) {
         printf("Error reading fixed information.\n");
-            exit(2);
+        exit(2);
     }
 
     /* Get variable screen information */
-        if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo)) {
-                printf("Error reading variable information.\n");
-                exit(3);
-        }
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo)) {
+        printf("Error reading variable information.\n");
+        exit(3);
+    }
 
     /* Figure out the size of the screen in bytes */
     screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
-    printf("Screen size is %d\n",screensize);
+    printf("Screen size is %ld\n",screensize);
     printf("Vinfo.bpp = %d\n",vinfo.bits_per_pixel);
 
     /* Map the device to memory */
     fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED,fbfd, 0);
     if ((int64_t)fbp == -1) {
-            printf("Error: failed to map framebuffer device to memory.\n");
-            exit(4);
+        printf("Error: failed to map framebuffer device to memory.\n");
+        exit(4);
     }
     printf("The framebuffer device was mapped to memory successfully.\n");
-        
+
 }
 
 
@@ -65,8 +65,8 @@ void I_StartFrame (void)
 {
 
 }
-__attribute__((packed))
-struct Color
+
+struct __attribute__((packed)) Color
 {
     uint8_t b;
     uint8_t g;
