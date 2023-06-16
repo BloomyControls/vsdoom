2b700c95d3ca8f2092831275cea50a06ba246331
Will Eccles <william.r.eccles@bloomy.com>
2023-06-16 14:42:34 -0400

    vsdoom.c: call D_DoomMain() + D_DoomLoop()

--- src/vsdoom.c
+++ src/vsdoom.c
@@ -7,33 +7,26 @@
  * Generated Fri Jun 16 14:05:30 2023
  */
 
-#include "ni_modelframework.h"
+#include "d_main.h"
 #include "model.h"
+#include "ni_modelframework.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif /* __cplusplus */
+extern void D_DoomLoop(void);
 
 int32_t vsdoom_Initialize(void) {
-  /* TODO: Initialize your model here */
   return NI_OK;
 }
 
 int32_t vsdoom_Start(void) {
-  /* TODO: Prepare to start your model here */
+  D_DoomMain();
   return NI_OK;
 }
 
 int32_t vsdoom_Step(const Inports* inports, double timestamp) {
-  /* TODO: Perform model steps here */
+  D_DoomLoop();
   return NI_OK;
 }
 
 int32_t vsdoom_Finalize(void) {
-  /* TODO: Cleanup your model here */
   return NI_OK;
 }
-
-#ifdef __cplusplus
-} /* extern "C" */
-#endif /* __cplusplus */
